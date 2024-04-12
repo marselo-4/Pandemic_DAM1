1718e273fba311d35c8f301ee982aeb60be22061
LujanIdn <lujacastillo2015@alumnes.ilerna.com>
2024-04-12 16:17:50 +0200

    Update BinEnfermedades.java

--- src/programa/BinEnfermedades.java
+++ src/programa/BinEnfermedades.java
@@ -10,17 +10,24 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Iterator;
 import java.util.Scanner;
 
 public class BinEnfermedades {
-	public static ArrayList<Ciudades> ciudades = new ArrayList<>();	
 
+	public BinEnfermedades() {
+		// TODO Auto-generated constructor stub
+	}
 
 	public static void main(String[] args) {
-
+		
+		//Obrir el fitxer bin y llegirt la primera linea String per veure les instruccions
+		
+		
+		//Despres printear totes les ciutats amb la seva enfermetat corresponent.
+		
+		
 		
 		String nombreFichero = "CCP.bin";
 		
@@ -64,10 +71,84 @@ public class BinEnfermedades {
 			System.out.println("Ha habido un error de lectura " + e);
 		}
 		
-		//--------------------------------------------------------------------------------------
-		
-		String linea = "";
+		String nombreFichero3 = "ciudades.txt";
 
+		try {
+			// Creamos un objeto de tipo FileReader para abrir un fichero de lectura
+			FileReader fileReader = new FileReader(nombreFichero);
+			// Utilizamos el Buffered para recibir lo que hay en el fichero y transformarlo
+			// en c�digo Java
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String linea = null;
+			do {
+				// Leemos el contenido del fichero
+				linea = bufferedReader.readLine();
+				
+				// Si no ha acabado de leer el fichero, printamos el valor de l�nea
+				if (linea != null) {
+					System.out.println(linea);
+				}
+			} while (linea != null);
+
+			bufferedReader.close();
+			fileReader.close();
+
+		} catch (IOException e) {
+			System.out.println("Ha habido un error al intentar abrir el fichero" + e);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		String nombreFichero2 = "ciudades-enfermedades.bin";
+		
+		try {
+			//Lo utilizamos para crear el fichero a partir del nombre
+			FileOutputStream fileOutputStream = new FileOutputStream(nombreFichero2);
+			//Este objeto traduce todos los datos de Java a binario
+			DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream);
+			
+//			dataOutputStream.writeUTF();
+//			
+//			
+//			dataOutputStream.writeInt();
+//			
+//	
+//			dataOutputStream.writeFloat();
+			
+			dataOutputStream.close();
+			fileOutputStream.close();
+		//Capturamos el error cuando el fichero indicado no exista
+		} catch (FileNotFoundException e1) {
+			System.out.println("Fichero no encontrado " + e1);
+		//Capturamos el error si ha habido un error de escrtura
+		} catch (IOException e) {
+			System.out.println("Error en la escritura del fichero " + e);
+		//Capturamos el error si en el scanner nos env�an una letra en vez de un n�mero en el nextInt()
+		} catch (InputMismatchException e2) {
+			System.out.println("Ha habido al leer los datos del usuario");
+		//Capturamos cualquier otra excepci�n
+		} catch (Exception e3) {
+			System.out.println("Ha habido un error no controlado " + e3);
+		}
 		
 	}
 
@@ -77,6 +158,9 @@ public class BinEnfermedades {
 //del mapa para el que estan ajustadas las coordenadas. Los datos de las enfermedades están ordenados: 
 //codigoEnfermedad (int), nombreEnfermedad(String) y colorEnfermeda (String) para las 4 enfermedades.
 //Después están las cordenadas x y que son dos (int)
+
+
+
 
 
 
