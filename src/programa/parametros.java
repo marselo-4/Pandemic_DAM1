1d32ed28c1d69336be046cd882e4cf8375d3c569
LujanIdn <lujacastillo2015@alumnes.ilerna.com>
2024-04-16 18:34:26 +0200

    Try modificar parametros.xml

--- src/programa/parametros.java
+++ src/programa/parametros.java
@@ -4,51 +4,97 @@ import java.io.File;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+
 public class parametros {
+	private static String fileName = "parametros.xml";
 
 	public static void main(String[] args) {
+
 		
+		//modificarXML();  //No executar si executes te borre el xml y el fa de 0 amb el codi que li dones.
+		imprimirXMLconsola();
+	}
 
+	public static void imprimirXMLconsola() {
+
+		try {
+			File inputFile = new File(fileName);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);
+			doc.getDocumentElement().normalize();
+
+			System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+
+			NodeList nodeList = doc.getElementsByTagName("dificultad");
+
+			for (int i = 0; i < nodeList.getLength(); i++) {
+				Node node = nodeList.item(i);
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+					Element element = (Element) node;
+					String id = element.getAttribute("id");
+					String porcentaje_vacuna = element.getElementsByTagName("Porcentaje_vacuna").item(0)
+							.getTextContent();
+					String brotes_maximos = element.getElementsByTagName("Brotes_maximos").item(0).getTextContent();
+					String propagacion_turno = element.getElementsByTagName("Propagacion_turno").item(0)
+							.getTextContent();
+					String infeccion_inicial = element.getElementsByTagName("Infeccion_inicial").item(0)
+							.getTextContent();
+
+					System.out.println("----------------------");
+					System.out.println("-Dificultad --> " + id);
+					System.out.println("-% de desarollo por accion --> " + porcentaje_vacuna);
+					System.out.println("-Brotes maximos antes de derrota --> " + brotes_maximos);
+					System.out.println("-Propagacion a ciudades por turno --> " + propagacion_turno);
+					System.out.println("-Ciudades infectadas al inicio --> " + infeccion_inicial);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void modificarXML() {
+		
 		 try {
-	            File inputFile = new File("parametros.xml");
-	            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-	            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-	            Document doc = dBuilder.parse(inputFile);
-	            doc.getDocumentElement().normalize();
+	            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+	            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
-	            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+	            Document doc = docBuilder.newDocument();
+	            Element rootElement = doc.createElement("parametros");
+	            doc.appendChild(rootElement);
 
-	            NodeList nodeList = doc.getElementsByTagName("dificultad");
+	            Element item = doc.createElement("dificultad");
+	            rootElement.appendChild(item);
 
-	            for (int i = 0; i < nodeList.getLength(); i++) {
-	                Node node = nodeList.item(i);
-	                if (node.getNodeType() == Node.ELEMENT_NODE) {
-	                    Element element = (Element) node;
-	                    String id = element.getAttribute("id");
-	                    String porcentaje_vacuna = element.getElementsByTagName("Porcentaje_vacuna").item(0).getTextContent();
-	                    String brotes_maximos = element.getElementsByTagName("Brotes_maximos").item(0).getTextContent();
-	                    String propagacion_turno = element.getElementsByTagName("Propagacion_turno").item(0).getTextContent();
-	                    String infeccion_inicial = element.getElementsByTagName("Infeccion_inicial").item(0).getTextContent();
+	            Element name = doc.createElement("name");
+	            name.appendChild(doc.createTextNode("Product ABC"));
+	            item.appendChild(name);
 
-	                    System.out.println("----------------------");
-	                    System.out.println("-Dificultad --> " + id);
-	                    System.out.println("-% de desarollo por accion --> " + porcentaje_vacuna);
-	                    System.out.println("-Brotes maximos antes de derrota --> " + brotes_maximos);
-	                    System.out.println("-Propagacion a ciudades por turno --> " + propagacion_turno);
-	                    System.out.println("-Ciudades infectadas al inicio --> " + infeccion_inicial);
-	                }
-	            }
+	    
+	            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	            Transformer transformer = transformerFactory.newTransformer();
+	            DOMSource source = new DOMSource(doc);
+	            StreamResult result = new StreamResult(fileName);
+
+	            transformer.transform(source, result);
+
+	            System.out.println("XML file saved successfully.");
+
 	        } catch (Exception e) {
 	            e.printStackTrace();
 	        }
-		 
-		 //Hasta aqui lo que hace es abrir el archivo parametros.xml y printear los datos
+			
 		
 	}
 
