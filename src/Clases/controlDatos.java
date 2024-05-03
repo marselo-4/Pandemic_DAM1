415a3ce5bf93baefb21353ad1eee12044eb83869
LujanIdn <lujancastillo2015@alumnes.ilerna.com>
2024-05-03 12:41:15 +0200

    Añadido libreria sql + modelo insert

--- src/Clases/controlDatos.java
+++ src/Clases/controlDatos.java
@@ -1,13 +1,48 @@
+
 package Clases;
+
+import java.sql.*;
+import java.sql.SQLException;
 
 public class controlDatos {
 
-	private String url = "jdbc:oracle:thin:@192.168.3.26:1521:xe"; // testing a casa cambiar ip a  jdbc:oracle:thin:@oracle.ilera.com:1521:xe
-	private String USER = "DAM1_2324_ALE_LUJAN";
-	private String password = "Lujan1234.";
+	private static String URL = "jdbc:oracle:thin:@oracle.ilerna.com:1521:xe"; // testing a casa cambiar ip a  jdbc:oracle:thin:@oracle.ilera.com:1521:xe
+	private static String USER = "DAM1_2324_ALE_LUJAN";
+	private static String PWD = "Lujan1234.";
 	private String ficheroTxt;
 	private String ficheroBin;
 	private static String ficheroXML = "parametros.xml";
+	
+	public static void main(String[] args) throws SQLException {
+		Connection con = conectarBaseDatos();
+
+		if (con != null) {
+			guardarPartida(con);
+			//select(con);
+			con.close();
+		}
+
+	}
+	
+	private static Connection conectarBaseDatos() {
+		Connection con = null;
+
+		System.out.println("Intentando conectarse a la base de datos");
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection(URL, USER, PWD);
+		} catch (ClassNotFoundException e) {
+			System.out.println("No se ha encontrado el driver " + e);
+		} catch (SQLException e) {
+			System.out.println("Error en las credenciales o en la URL " + e);
+		}
+
+		System.out.println("Conectados a la base de datos");
+
+		return con;
+	}
+	
 	
 	public static void cargarCiudades() {
 		
@@ -21,7 +56,11 @@ public class controlDatos {
 		
 	}
 	
-	public static void cargarPartida() {
+
+	
+
+	public static void cargarPartida(Connection con) {
+		
 	// El boto de cargar partida del menu te ha de deixar elegit entre tta la llista de guardados
 		
 		//setar l'estat de totes les ciutats
@@ -29,9 +68,12 @@ public class controlDatos {
 		//setear l'estat de totes les vacunes
 		
 		//setear el nº de brotes actius
+		
+		
+		
 	}
 	
-	public static void guardarPartida() {
+	public static void guardarPartida(Connection con) {
 		// Un boto de guardar partida sempre visible en pantalla ha d fer saltar esta funcio
 		
 		//Guardar l'estat de totes les ciutats
@@ -40,6 +82,16 @@ public class controlDatos {
 		
 		//Guardar el nº de brotes actius
 		
+		String sql = "INSERT INTO PartidasGuardadas VALUES( 69, 'Juan', 10, null, 2, 9 , VACUNA('Azul', 30),  VACUNA('Rojo', 40), VACUNA('Amarillo', 20), VACUNA('Verde', 10), Lista_ciudades(CIUDAD('San Francisco', 3), CIUDAD('Atlanta', 1)))";
+				
+		try {
+			Statement st = con.createStatement();
+			st.execute(sql);
+			
+			System.out.println("Insert registrado correctamente");
+		} catch (SQLException e) {
+			System.out.println("Ha habido un error en el Insert " + e);
+		}
 		
 	}
 	
@@ -52,11 +104,11 @@ public class controlDatos {
 	}
 
 	public String getUrl() {
-		return url;
+		return URL;
 	}
 
 	public void setUrl(String url) {
-		this.url = url;
+		this.URL = url;
 	}
 
 	public String getUser() {
@@ -68,11 +120,11 @@ public class controlDatos {
 	}
 
 	public String getPassword() {
-		return password;
+		return PWD;
 	}
 
 	public void setPassword(String password) {
-		this.password = password;
+		this.PWD = password;
 	}
 
 	public String getFicheroTxt() {
@@ -101,23 +153,3 @@ public class controlDatos {
 
 }
 
-
-/*
- * CREATE OR REPLACE TYPE VACUNA AS OBJECT (
-color VARCHAR2(20),
-porcentage NUMBER(3)
-);
-
-CREATE OR REPLACE TYPE CIUDAD AS OBJECT (
-nombre VARCHAR2(20),
-infeccion NUMBER(1)
-);
-
-CREATE TABLE PartidasGuardadas (
-NPartida NUMBER(4) PRIMARY KEY,
-FECHA DATE DEFAULT SYSDATE,
-azul VACUNA,
-rojo VACUNA,
-amarillo VACUNA,
-verde VACUNA
-);*/
